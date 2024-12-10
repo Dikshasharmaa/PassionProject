6ba13cba508d737e9d0626aea66029444597cc12
Diksha Sharma <diksha66sharma78@gmail.com>
2024-12-10 16:21:24 -0500

    Getting value by getting it throug searchValue func

--- kitchen_stories/src/Component/Mainpage/Mainpage.js
+++ kitchen_stories/src/Component/Mainpage/Mainpage.js
@@ -4,8 +4,14 @@ import './Mainpage.css';
 
 const Mainpage = () => {
   const [data, setData] = useState();
+  const [input, setInput] = useState();
+
+  const searchValue = (event) => {
+    setInput(event.target.value)
+
+  }
   const myFun = async () => {
-    const info = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=cake`);
+    const info = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`);
     const jsonData = await info.json()
     console.log(jsonData.meals);
     setData(jsonData.meals)
@@ -15,7 +21,7 @@ const Mainpage = () => {
     <>
     <div className = 'container' >
         <div className='searchBar'>
-            <input type='text' placeholder='What do yo want to eat today!'/>
+            <input type='text' placeholder='What do yo want to eat today!' onChange={searhValue}/>
             <button onClick={myFun}>Search</button>
         </div>
     </div>
