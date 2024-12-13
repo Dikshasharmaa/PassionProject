0dce9e80b44cfcb90e4f3b827a4d946fc8a53df3
Diksha Sharma <diksha66sharma78@gmail.com>
2024-12-13 17:44:03 -0500

    Added error msg when the user search without anything typed

--- kitchen_stories/src/Component/Mainpage/Mainpage.js
+++ kitchen_stories/src/Component/Mainpage/Mainpage.js
@@ -3,19 +3,30 @@ import './Mainpage.css';
 import MealCard from '../MealCard/MealCard';
 
 
+
 const Mainpage = () => {
   const [data, setData] = useState();
-  const [input, setInput] = useState();
+  const [input, setInput] = useState("");
+  const[msg, setmsg] = useState("");
 
   const searchValue = (event) => {
     setInput(event.target.value)
 
   }
   const myFun = async () => {
-    const info = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`);
+    if(input == ""){
+      setmsg("Please enter something")
+
+    }
+    else{
+      const info = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`);
     const jsonData = await info.json()
     // console.log(jsonData.meals);
     setData(jsonData.meals)
+    setmsg("")
+
+    }
+    
   }
   // console.log(data);
   return (
@@ -25,6 +36,7 @@ const Mainpage = () => {
             <input type='text' placeholder='What do yo want to eat today!' onChange={searchValue}/>
             <button onClick={myFun}>Search</button>
         </div>
+        <h4 className='msg'>{msg}</h4>
         <div>
           <MealCard detail ={data}/>
         </div>
