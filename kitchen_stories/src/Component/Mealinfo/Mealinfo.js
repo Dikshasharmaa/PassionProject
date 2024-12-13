ba864aa64527eb09f60f0796cadf525ec54b98b9
Diksha Sharma <diksha66sharma78@gmail.com>
2024-12-13 16:34:23 -0500

    Getting the info from Api for the specific id

--- kitchen_stories/src/Component/Mealinfo/Mealinfo.js
+++ kitchen_stories/src/Component/Mealinfo/Mealinfo.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { useParams } from 'react-router'
+import { json } from 'stream/consumers';
 
 const Mealinfo = () => {
     const {mealid} = useParams();
     console.log(mealid);
+    const getInfo = async () =>{
+      const get = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i= ${mealid}`);
+      const jsonData = await get.json();
+      console.log(jsonData);
+    }
+    if(mealid != ""){
+      getInfo()
+    }
   return (
     <div>
         Mealinfo
